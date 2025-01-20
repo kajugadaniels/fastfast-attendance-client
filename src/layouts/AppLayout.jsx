234911f3d03263bd69d38597d9aea96181a030bc
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-20 17:44:59 +0200

    Setup app layout

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { Header, Sidebar } from '../components'
+import { Header, MobileMenu, Sidebar } from '../components'
 
 const AppLayout = () => {
     return (
         <div class="rubick px-5 sm:px-8 py-5 before:content-[''] before:bg-gradient-to-b before:from-theme-1 before:to-theme-2 dark:before:from-darkmode-800 dark:before:to-darkmode-800 before:fixed before:inset-0 before:z-[-1]">
-            <Header />
+            <MobileMenu />
 
-            <Sidebar />
+            <div className="mt-[4.7rem] flex md:mt-0">
+                <Sidebar />
 
-            <Outlet />
+                <Outlet />
+            </div>
         </div>
     )
 }
