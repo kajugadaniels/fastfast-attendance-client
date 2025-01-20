f1e43507f3cf8fb2f82f0fc1e50fa604532ef43e
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-21 00:55:50 +0200

    New changes

--- src/pages/employees/ShowEmployee.jsx
+++ src/pages/employees/ShowEmployee.jsx
@@ -1,8 +1,21 @@
+import { Eye } from 'lucide-react'
 import React from 'react'
 
 const ShowEmployee = () => {
     return (
-        <div>ShowEmployee</div>
+        <>
+            <div className="intro-y col-span-12 mt-8 flex flex-wrap items-center xl:flex-nowrap">
+                <h2 className="mr-auto text-lg font-medium">
+                    Employee Details
+                </h2>
+                <a href='/employees' className="transition duration-200 border inline-flex items-center justify-center py-2 px-3 rounded-md font-medium cursor-pointer focus:ring-4 focus:ring-primary focus:ring-opacity-20 focus-visible:outline-none dark:focus:ring-slate-700 dark:focus:ring-opacity-50 [&:hover:not(:disabled)]:bg-opacity-90 [&:hover:not(:disabled)]:border-opacity-90 [&:not(button)]:text-center disabled:opacity-70 disabled:cursor-not-allowed bg-primary border-primary text-white dark:border-primary mr-2 shadow-md">
+                    Go Back
+                    <span className="flex h-5 w-5 items-center justify-center">
+                        <Eye className="stroke-1.5 h-4 w-4" />
+                    </span>
+                </a>
+            </div>
+        </>
     )
 }
 
