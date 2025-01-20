665c653eb771a86767d685dd204dfde0b1b603dc
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-20 17:01:34 +0200

    Setup auth login page

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import {  } from './pages'
 import { Route, Routes } from 'react-router-dom'
 import AppLayout from './layouts/AppLayout'
-import { Dashboard } from './pages'
+import { Dashboard, Login } from './pages'
 
 const AppRoutes = () => {
     return (
         <Routes>
+            <Route path='/' element={<Login />} />
             <Route element={<AppLayout />}>
                 <Route path="/dashboard" element={<Dashboard />} />
             </Route>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,3 @@
+export { default as Login } from './auth/Login'
+
 export { default as Dashboard } from './Dashboard'
