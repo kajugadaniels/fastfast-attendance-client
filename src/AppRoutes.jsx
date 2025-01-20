aa7955f45a8078dd1c96d793c3c0d04b81477af7
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-20 17:56:32 +0200

    Setup profile page

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NotFound } from './pages'
+import { NotFound, Profile } from './pages'
 import { Route, Routes } from 'react-router-dom'
 import AppLayout from './layouts/AppLayout'
 import { Dashboard, Login } from './pages'
@@ -9,6 +9,7 @@ const AppRoutes = () => {
         <Routes>
             <Route path='/' element={<Login />} />
             <Route element={<AppLayout />}>
+                <Route path="/profile" element={<Profile />} />
                 <Route path="/dashboard" element={<Dashboard />} />
             </Route>
             <Route path='*' element={<NotFound />} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export { default as Login } from './auth/Login'
+export { default as Profile } from './auth/Profile'
 
 export { default as Dashboard } from './Dashboard'
 
