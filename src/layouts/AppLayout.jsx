02c66b5e668c2c3dd09690e4066765e70f44a97d
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-20 17:02:54 +0200

    Setup components

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Header, Sidebar } from '../components'
 
 const AppLayout = () => {
     return (
         <div>
-            AppLayout
+            <Header />
+
+            <Sidebar />
 
             <Outlet />
         </div>
