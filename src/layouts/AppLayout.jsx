5f06aaae4006b409f3ad410f34c9f4a699161f20
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-20 17:40:10 +0200

    Setup mobile menu

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -4,7 +4,7 @@ import { Header, Sidebar } from '../components'
 
 const AppLayout = () => {
     return (
-        <div>
+        <div class="rubick px-5 sm:px-8 py-5 before:content-[''] before:bg-gradient-to-b before:from-theme-1 before:to-theme-2 dark:before:from-darkmode-800 dark:before:to-darkmode-800 before:fixed before:inset-0 before:z-[-1]">
             <Header />
 
             <Sidebar />
