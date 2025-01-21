b535746d7d444a4b0cc2b87cbf64939b66a42608
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-21 16:30:24 +0200

    Setup error handler in api.js

---
+++ src/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const baseURL =
+    import.meta.env.MODE === 'production'
+        ? import.meta.env.VITE_API_BASE_URL_PROD
+        : import.meta.env.VITE_API_BASE_URL;
+
+const apiClient = axios.create({
+    baseURL,
+});
+
+apiClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Token ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+apiClient.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
