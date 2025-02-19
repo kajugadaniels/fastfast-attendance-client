5ba156728a3d2828a3d9c6f0f090fdb895ec6749
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-19 23:25:16 +0200

    New changes

--- src/pages/employees/ShowEmployee.jsx
+++ src/pages/employees/ShowEmployee.jsx
@@ -62,7 +62,7 @@ const ShowEmployee = () => {
     };
 
     // QR Code generation logic using employee_id
-    const qrCodeValue = `https://nexcode-attendance.vercel.app/scan/${employeeData?.employee?.id}`;
+    const qrCodeValue = `https://nexcode-attendance.vercel.app/employee/${employeeData?.employee?.id}`;
 
     const downloadQRCode = () => {
         if (qrCodeRef.current) {
