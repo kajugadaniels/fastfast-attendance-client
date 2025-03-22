58f087907039d9b0b32985b3fd5f469c579ed8e3
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-22 15:57:10 +0200

    New changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -77,7 +77,7 @@ const Header = () => {
                     onClick={handleToggleDropdown}
                 >
                     <img
-                        src="https://midone-html.left4code.com/dist/images/fakers/profile-6.jpg"
+                        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
                         alt=""
                     />
                 </button>
